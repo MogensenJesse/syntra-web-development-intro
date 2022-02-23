9ffd1dcef58a984f078bfd0c1d9a0a4f0ba79d13
MogensenJesse <71662157+MogensenJesse@users.noreply.github.com>
2022-02-23 22:01:35 +0100

    REST api

---
+++ 3. Intro to Express/2. Crud API/crud.js
@@ -0,0 +1,45 @@
+const { initializeApp } = require("firebase/app");
+const { getDatabase, ref, set, get, child, remove } = require("firebase/database");
+const uuidv4 = require("uuid").v4;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDgdrurQiWH039djC7IqLmkzgKVNP5Qkis",
+  authDomain: "countries-1c733.firebaseapp.com",
+  databaseURL: "https://countries-1c733-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "countries-1c733",
+  storageBucket: "countries-1c733.appspot.com",
+  messagingSenderId: "424730380692",
+  appId: "1:424730380692:web:b386550ed7bce1beb7a781",
+};
+
+const app = initializeApp(firebaseConfig);
+
+// Get a reference to the database service
+const database = getDatabase(app);
+
+module.exports = {
+  writeItem: (item) => {
+    const id = uuidv4();
+    set(ref(database, "countries/" + id), item);
+  },
+  updateItem: (id, item) => {
+    set(ref(database, "countries/" + id), item);
+  },
+  readItem: async (itemId) => {
+    const dbRef = ref(database);
+    const snapshot = await get(child(dbRef, `countries/${itemId}`));
+    if (snapshot.exists()) {
+      return snapshot.val();
+    }
+  },
+  readAll: async () => {
+    const dbRef = ref(database);
+    const snapshot = await get(child(dbRef, `countries`));
+    if (snapshot.exists()) {
+      return Object.values(snapshot.val());
+    }
+  },
+  removeItem: async (itemId) => {
+    await remove(ref(database, `countries/${itemId}`));
+  },
+};
--- 3. Intro to Express/2. Crud API/index.js
+++ 3. Intro to Express/2. Crud API/index.js
@@ -1,41 +1,78 @@
 const dotenv = require("dotenv");
 const express = require("express");
-const HttpError = require("./errors/HttpError");
 const swaggerUi = require("swagger-ui-express");
+const HttpError = require("./errors/HttpError");
 const swaggerDocument = require("./docs/swagger.json");
+const { readItem, readAll, removeItem } = require("./crud");
 
-dotenv.config();
+dotenv.config(); // wordt standaard uit root gehaald
 
 const api = express();
 const port = process.env.PORT;
 
 api.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+// if you want to use req.body
+api.use(express.json());
+
+api.get("/countries/:id", (req, res, next) => {
+  const id = req.params.id;
+  return readItem(id)
+    .then((val) => res.json(val))
+    .catch((err) => next(new HttpError(500, err.message)));
+});
+
+api.get("/countries", (req, res, next) => {
+  return readAll()
+    .then((val) => res.json(val))
+    .catch((err) => next(new HttpError(500, err.message)));
+});
+
+api.delete("/countries", (req, res, next) => {
+  console.log(id)
+  return removeItem(id)
+    .then((val) => {
+      res.status(200).json({ success: "Welp, entry deleted" });
+    })
+    .catch((err) => next(new HttpError(500, err.message)));
+});
+
+api.post("/countries/:id", (req, res, next) => {
+  const body = req.body;
+  return writeItem(body)
+    .then((val) => {
+      res.status(200).json({ success: `Stored with ${id}` });
+    })
+    .catch((err) => next(new HttpError(500, err.message)));
+});
+
+
+
+// middleware
 api.get(
   "/",
-  async (req, res, next) => {
-    console.log("heavy data prep");
-    await firebase.get();
-    req.fdglkj = await firebase.get();
-    await sap.get();
-    console.log("heavy data prep done.");
-    next();
+  (req, res, next) => {
+    console.log("preparations are being made");
+    req.preppedData = { name: "Hallo daar yo" }; // preppedData is random
+    console.log("preparations done");
+    next(); // next is indien er geen response wordt gegeven => door naar volgende middleware
   },
   (req, res, next) => {
     if (req.query.error) {
-      next(new HttpError(400, "Something is not right."));
+      next(new HttpError(400, "Dit klopt niet hoor mateke"));
     }
-    next();
+    next(); // next is indien er geen response wordt gegeven => door naar volgende middleware
   },
-  (req, res) => {
-    res.send(req.fdglkj);
+  (req, res, next) => {
+    res.json(req.preppedData);
   }
 );
 
 api.use("*", (req, res, next) => {
-  next(new HttpError(404, "Route does not exist."));
+  next(new HttpError(404, "Deze url klopt niet hoor mateke"));
 });
 
+// error handling
 api.use((err, req, res, next) => {
   res.status(err.status).json({
     status: err.status,
@@ -44,5 +81,5 @@ api.use((err, req, res, next) => {
 });
 
 api.listen(port, () => {
-  console.log(`🚀 Country api listening at http://localhost:${port}`);
+  console.log(`API listening at http://localhost:${port}`);
 });
